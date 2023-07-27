8552772a7d3c24a9782f6c2780353507e103eb36
David Day <david.day2017@gmail.com>
2023-07-27 09:13:59 -0500

    add type info and client parameter to BaseChip

--- OpenEEPROM/chip/basechip.py
+++ OpenEEPROM/chip/basechip.py
@@ -1,20 +1,24 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from OpenEEPROM.client import OpenEEPROMClient
 
 
 class BaseChip(ABC):
-    def __init__(self, name, size):
+    def __init__(self, name: str, size: int, client: OpenEEPROMClient):
         self.name = name
         self.size = size
+        self.client = client
     
     @abstractmethod
-    def read(self, address, byte_count):
+    def read(self, address: int, byte_count: int) -> List[int]:
         pass
 
     @abstractmethod
-    def write(self, address, byte_list):
+    def write(self, address: int, byte_list: List[int]) -> int:
         pass
 
     @abstractmethod
-    def erase(self):
+    def erase(self) -> None:
         pass
 
