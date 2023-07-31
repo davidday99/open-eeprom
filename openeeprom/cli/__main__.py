df8fa9c2eb8e33651d2be39dcb5585ee7aeabdf9
David Day <david.day2017@gmail.com>
2023-07-31 00:47:51 -0500

    restructure CLI main script

--- openeeprom/cli/__main__.py
+++ openeeprom/cli/__main__.py
@@ -4,6 +4,7 @@ from openeeprom.chip import \
         microchip25lc320, \
         at28c256
 from openeeprom.transport.serial import SerialTransport
+from openeeprom.transport.tcp import TcpTransport
 from openeeprom.client import OpenEEPROMClient
 
 DESCRIPTION = '''
@@ -21,6 +22,7 @@ def parse_args():
     parser.add_argument('command', help='read, write, erase, verify, list')
     parser.add_argument('--chip', help="run command 'list' to view supported chips")
     parser.add_argument('--serial', type=str)
+    parser.add_argument('--tcp', type=str)
     parser.add_argument('--offset', type=str, default=0)
     parser.add_argument('--count', type=str)
     parser.add_argument('--file', type=str)
@@ -28,62 +30,87 @@ def parse_args():
     return args 
 
 
-def main():
-    args = parse_args()
-    
-    if args.command == 'list':
-        chips = SUPPORTED_DEVICES.keys()
-        print('Supported chips:')
-        print()
-        for chip in chips:
-            print(chip, '\t', SUPPORTED_DEVICES[chip].description)
-        return
-
+def init_transport(args):
     if args.serial:
         port, baud_rate = args.serial.split(':')
         baud_rate = int(baud_rate)
         transport = SerialTransport(port, baud_rate)
+    elif args.tcp:
+        hostname, port = args.tcp.split(':')
+        port = int(port)
+        transport = TcpTransport(hostname, port)
 
-    chip = SUPPORTED_DEVICES[args.chip]
-    client = OpenEEPROMClient(transport)
-    chip.connect(client)
+    return transport
 
-    if args.command == 'read':
-        offset = int(args.offset)
-        count = int(args.count) if args.count else chip.size
-        data = chip.read(offset, count)
-        
-        if args.file:
-            with open(args.file, 'wb') as f:
-                f.write(bytes(data))
-        else:
-            print(data)
+def do_list():
+    chips = SUPPORTED_DEVICES.keys()
+    print('Supported chips:')
+    print()
+    for chip in chips:
+        print(chip, '\t', SUPPORTED_DEVICES[chip].description)
 
-    if args.command == 'write':
-        offset = int(args.offset)
-        
-        with open(args.file, 'rb') as f:
-            data = list(f.read())
 
-        chip.write(offset, data)
-         
-    if args.command == 'erase':
-        chip.erase()
-        print('Chip erase complete.')
+def do_read(chip, args):
+    offset = int(args.offset)
+    count = int(args.count) if args.count else chip.size
+    data = chip.read(offset, count)
+    
+    if args.file:
+        with open(args.file, 'wb') as f:
+            f.write(bytes(data))
+    else:
+        print(data)
 
-    if args.command == 'verify':
-        with open(args.file, 'rb') as f:
-            data = list(f.read())
 
-        chip_contents = chip.read(0, len(data))
+def do_write(chip, args):
+    offset = int(args.offset)
+    
+    with open(args.file, 'rb') as f:
+        data = list(f.read())
 
-        for idx, pair in enumerate(zip(data, chip_contents)):
-            if pair[0] != pair[1]:
-                print(f'Difference at offset {idx}. {pair[0]} (file) != {pair[1]} (chip).')
+    chip.write(offset, data)
 
-        print('Contents are equivalent.')
+
+def do_erase(chip):
+    chip.erase()
+    print('Chip erase complete.')
+
+
+def do_verify(chip, args):
+    with open(args.file, 'rb') as f:
+        data = list(f.read())
+
+    chip_contents = chip.read(0, len(data))
+
+    for idx, pair in enumerate(zip(data, chip_contents)):
+        if pair[0] != pair[1]:
+            print(f'Difference at offset {idx}. {pair[0]} (file) != {pair[1]} (chip).')
+
+    print('Contents are equivalent.')
+
+
+def main():
+    args = parse_args()
+    
+    if args.command == 'list':
+        do_list()
+    else:
+        transport = init_transport(args)
+
+        chip = SUPPORTED_DEVICES[args.chip]
+        client = OpenEEPROMClient(transport)
+        chip.connect(client)
+
+        if args.command == 'read':
+            do_read(chip,args)
+        elif args.command == 'write':
+            do_write(chip, args) 
+        elif args.command == 'erase':
+            do_erase(chip)
+        elif args.command == 'verify':
+            do_verify(chip, args)
 
 
 if __name__ == '__main__':
     main()
-None
+
